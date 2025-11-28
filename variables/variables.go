338575f3516b7b93b4bc044c06bfe6e4c64f52a0
susankoju <aksusankoju@gmail.com>
2025-11-28 17:54:30 +0100

    Add variables

---
+++ variables/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "1"
+	fmt.Println(a)
+
+	var b int = 2
+	fmt.Println(b)
+
+	var c, d int
+	c = 3
+	d = 4
+	fmt.Println(c + d)
+
+	bo := false
+	fmt.Println(!bo)
+}
