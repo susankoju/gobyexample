f1604ee7ca6c6c67ff2ed75cc52b53e4396e687a
susankoju <aksusankoju@gmail.com>
2025-11-28 09:34:49 +0100

    Add values

---
+++ values/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go " + "lang")
+	fmt.Println("1 + 3 =", 1+3)
+	fmt.Println("22 / 7 =", 22/7)
+	fmt.Println("22.0 / 7.0 =", 22.0/7.0)
+	fmt.Println("true && false = ", true && false)
+	fmt.Println("true || false = ", true || false)
+	fmt.Println("!true = ", !true)
+}
