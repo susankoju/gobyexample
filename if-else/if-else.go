ba5b9ef9d4adecd8792614da30f3d35bc05f9ab2
susankoju <aksusankoju@gmail.com>
2025-11-29 02:10:53 +0100

    Add if else

---
+++ if-else/if-else.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 5
+	if num%3 == 0 {
+		fmt.Println("Divisible by 3")
+	} else {
+		fmt.Println("Not divisible by 3")
+	}
+
+	if num%5 == 0 || num%3 == 0 {
+		fmt.Println("Is divisible by 5 or 3")
+	}
+
+	if i := 0; i == 0 {
+		fmt.Println("i is 0")
+	}
+}
